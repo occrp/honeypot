759cefc26bed686f7a6573a9f3f6273cfe95be26
Michał 'rysiek' Woźniak <rysiek@hackerspace.pl>
2016-11-01 19:12:19 +0100

    now ready for testing

--- index.php
+++ index.php
@@ -20,16 +20,8 @@ function dbg($txt) {
   }
 }
 
-# specimen 
-define('SPECIMEN_DIR', 'specimens/' . $_SERVER["HTTP_HOST"]);
+# dump dir
 define('DUMP_DIR', 'dumps/' . $_SERVER["HTTP_HOST"]);
-
-# is the domain name within our scope?
-if (!file_exists(SPECIMEN_DIR) or !is_dir(SPECIMEN_DIR)) {
-  dbg('specimen dir does not exist - ' . SPECIMEN_DIR);
-  header('HTTP/1.1 500 Internal Server Error');
-  exit(0);
-}
 
 
 # getting the specimen headers
@@ -57,55 +49,9 @@ function output_specimen_html($f) {
   dbg("specimen-file $f"); # DEBUG
   # get the specimen
   $html = file_get_contents($f);
-  # replace whatever is there to be replaced, if there is anything to be replaced, that is
-  $html = replace_credentials($html);
   # print the damn thing
   dbg("printing out html"); # DEBUG
   print($html);
-}
-
-
-# replacing everything that needs to be replaced
-function replace_credentials($str) {
-  dbg("handling credentials...");
-  # we only operate when POST is in effect
-  if($_SERVER['REQUEST_METHOD'] === 'GET') return $str;
-  # get the creds, if available
-  $user = "";
-  $mail = "";
-  $pass = "";
-  
-  # that one's simple
-  if (isset($_POST['pwd'])) {
-    $pass = $_POST['pwd'];
-    dbg("pass: $pass");
-  }
-  
-  # if we have 'log' set, that's the username and we're done
-  if (isset($_POST['log'])) {
-    $user = $_POST['log'];
-    dbg("user from log: $user");
-    
-  # otherwise, if user_login does not contain '@', assume a user login
-  } elseif (isset($_POST['user_login'])) {
-    if (strpos($_POST['user_login'], '@') === false) {
-      $user = $_POST['user_login'];
-      dbg("user from user_login: $user");
-      
-    # otherwise assume e-mail
-    } else {
-      $mail = $_POST['user_login'];
-      dbg("email from user_login: $mail");
-    }
-  }
-  
-  # replace
-  dbg("replacing...");
-  $str = str_replace('HONEYUSER', $user, $str);
-  $str = str_replace('HONEYPASS', $pass, $str);
-  $str = str_replace('HONEYMAIL@EXAMPLE.COM', $mail, $str);
-  # return the string
-  return $str;
 }
 
 
@@ -129,90 +75,7 @@ function dump_request_data() {
 }
 
 dump_request_data();
-
-# is method GET or POST?
-if($_SERVER['REQUEST_METHOD'] === 'GET') {
-  dbg("GET!");
-  
-  # is the "action" get parameter set to "lostpassword?
-  if ( (isset($_GET['action'])) and ($_GET['action'] === 'lostpassword') ) {
-    dbg("lostpassword");
-    output_specimen_headers(SPECIMEN_DIR . '/lostpass.headers');
-    output_specimen_html(SPECIMEN_DIR . '/lostpass.html');
-  # nah, just a "clean" login site
-  } else {
-    dbg("clean");
-    output_specimen_headers(SPECIMEN_DIR . '/clean.headers');
-    output_specimen_html(SPECIMEN_DIR . '/clean.html');
-  }
-  
-# a bit more tricky, now!
-} elseif($_SERVER['REQUEST_METHOD'] === 'POST') {
-  dbg("POST!");
-  
-  # is it the lostpassword page?
-  if (isset($_GET['action']) and ($_GET['action'] === 'lostpassword') ) {
-    dbg("lostpassword");
-    
-    # okay, do we have *anything* in POST?
-    if (isset($_POST['user_login']) and ! empty($_POST['user_login'])) {
-      dbg("user_login present");
-      
-      # is it an e-mail?
-      if (strpos($_POST['user_login'], '@') !== false) {
-        dbg("user_login is email");
-        output_specimen_headers(SPECIMEN_DIR . '/lostpass_email.headers');
-        output_specimen_html(SPECIMEN_DIR . '/lostpass_email.html');
-      
-      # user login, apparently!
-      } else {
-        dbg("user_login is username");
-        output_specimen_headers(SPECIMEN_DIR . '/lostpass_user.headers');
-        output_specimen_html(SPECIMEN_DIR . '/lostpass_user.html');
-      }
-    
-    # nah, just a clean page pls
-    } else {
-      dbg("clean lostpass plz");
-      output_specimen_headers(SPECIMEN_DIR . '/lostpass.headers');
-      output_specimen_html(SPECIMEN_DIR . '/lostpass.html');
-    }
-  
-  # it's the login page, then,
-  } else {
-    dbg("login page");
-  
-    # do we have the user?
-    if (isset($_POST['log']) and ! empty($_POST['log'])) {
-      dbg("log present");
-      
-      # do we also have the password?
-      if (isset($_POST['pwd']) and ! empty($_POST['pwd'])) {
-        dbg("pwd present");
-        # both!
-        output_specimen_headers(SPECIMEN_DIR . '/userpass.headers');
-        output_specimen_html(SPECIMEN_DIR . '/userpass.html');
-      } else {
-        # just the user
-        output_specimen_headers(SPECIMEN_DIR . '/user.headers');
-        output_specimen_html(SPECIMEN_DIR . '/user.html');
-      }
-      
-    } elseif (isset($_POST['pwd'])  and ! empty($_POST['pwd'])) {
-      dbg("pwd present");
-      # just the pasword
-      output_specimen_headers(SPECIMEN_DIR . '/password.headers');
-      output_specimen_html(SPECIMEN_DIR . '/password.html');
-      
-    # nothing is supplied, just display a clean login page
-    } else {
-      dbg("nothing present, clean login page");
-      output_specimen_headers(SPECIMEN_DIR . '/clean.headers');
-      output_specimen_html(SPECIMEN_DIR . '/clean.html');
-    }
-    
-  }
-  
-}
+output_specimen_headers('504.headers');
+output_specimen_html('504.html');
 
 ?>
